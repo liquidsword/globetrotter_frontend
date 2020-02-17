1f5c36af3bab0a27dc7061dcd561202740efdd0b
liquidsword <audenrobertson@gmail.com>
2020-02-16 19:10:07 -0500

    NavBar and MainContainer creation
    created a navbar component and a maincontainer component so I can add in my login and logout data and start manipulating my css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login.js';
-import Logout from './components/Logout.js'
 import { connect } from 'react-redux'
 import { getCurrentUser } from './actions/currentUser'
+import NavBar from './components/NavBar.js';
+import MainContainer from './components/MainContainer.js'
 
 class App extends React.Component{
 
@@ -13,16 +13,12 @@ class App extends React.Component{
 
   render () {
     return (
-      //think about putting this into a nav bar
-      this.props.currentUser ? <Login/> : <Logout/>
+      <div className = "App">
+        <NavBar/>
+        <MainContainer/>
+      </div>
     );
   }
 }
 
-const mapStateToProps = state({ currentUser }) = > {
-  return {
-    currentUser
-  }
-}
-
-export default connect(mapStateToProps, {getCurrentUser})(App);
+export default connect(null, { getCurrentUser })(App);
